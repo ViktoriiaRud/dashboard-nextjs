d076e11de2cbb7563ff9295211dd6398a891ce81
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-11-08 15:16:40 +0100

    Handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,8 +14,13 @@ const FormSchema = z.object({
 });
  
 export async function deleteInvoice(id: string) {
+  try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
   }
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
@@ -26,12 +31,20 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
 `;
 
 revalidatePath('/dashboard/invoices');
--- app/ui/invoices/[id]/edit/page.tsx
+++ app/ui/invoices/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
  
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -11,6 +13,8 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchCustomers(),
   ]);
 
+  if (!invoice) {
+    notFound();
 
   return (
     <main>
